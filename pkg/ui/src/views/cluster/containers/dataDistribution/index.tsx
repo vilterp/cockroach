50e4644c12b580bafd3334f87e8a75114c9f0d9f
Pete Vilter <vilterp@cockroachlabs.com>
2018-06-29 19:23:13 -0400

    ui: render range log on data distribution page
    needs a lot of styling work
    
    Release note: None

--- pkg/ui/src/views/cluster/containers/dataDistribution/index.tsx
+++ pkg/ui/src/views/cluster/containers/dataDistribution/index.tsx
@@ -2,6 +2,7 @@ import _ from "lodash";
 import React from "react";
 import { createSelector } from "reselect";
 import { connect } from "react-redux";
+import * as Long from "long";
 import Helmet from "react-helmet";
 
 import Loading from "src/views/shared/components/loading";
@@ -9,14 +10,22 @@ import spinner from "assets/spinner.gif";
 import { ToolTipWrapper } from "src/views/shared/components/toolTip";
 import * as docsURL from "src/util/docs";
 import { FixLong } from "src/util/fixLong";
+import { TimestampToMoment } from "src/util/convert";
 import { cockroach } from "src/js/protos";
 import { AdminUIState } from "src/redux/state";
-import { refreshDataDistribution, refreshNodes, refreshLiveness } from "src/redux/apiReducers";
+import {
+  refreshDataDistribution,
+  refreshNodes,
+  refreshLiveness,
+  refreshRangeLog,
+} from "src/redux/apiReducers";
 import { LocalityTree, selectLocalityTree } from "src/redux/localities";
 import ReplicaMatrix, { SchemaObject } from "./replicaMatrix";
 import { TreeNode, TreePath } from "./tree";
+import { RangeLogResponseMessage } from "src/util/api";
 import "./index.styl";
 
+import RangeLogRequest = cockroach.server.serverpb.RangeLogRequest;
 type DataDistributionResponse = cockroach.server.serverpb.DataDistributionResponse;
 type NodeDescriptor = cockroach.roachpb.NodeDescriptor$Properties;
 type ZoneConfig$Properties = cockroach.server.serverpb.DataDistributionResponse.ZoneConfig$Properties;
@@ -33,6 +42,7 @@ interface DataDistributionProps {
   dataDistribution: DataDistributionResponse;
   localityTree: LocalityTree;
   sortedZoneConfigs: ZoneConfig$Properties[];
+  rangeLog: RangeLogResponseMessage;
 }
 
 class DataDistribution extends React.Component<DataDistributionProps> {
@@ -50,6 +60,33 @@ class DataDistribution extends React.Component<DataDistributionProps> {
             </li>
           ))}
         </ul>
+      </div>
+    );
+  }
+
+  renderRangeLog() {
+    return (
+      <div>
+        <table className="range-log">
+          <thead>
+            <tr className="range-log__row--header">
+              <th className="range-log__cell">Timestamp</th>
+              <th className="range-log__cell">Event Type</th>
+              <th className="range-log__cell">Event</th>
+              <th className="range-log__cell">Pretty Info</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.rangeLog.events.map((rangeLogEvent) => (
+              <tr key={rangeLogEvent.event.timestamp.seconds.toString()} className="range-log__row--body">
+                <td className="range-log__cell">{TimestampToMoment(rangeLogEvent.event.timestamp).toString()}</td>
+                <td className="range-log__cell">{cockroach.storage.RangeLogEventType[rangeLogEvent.event.event_type]}</td>
+                <td className="range-log__cell range-log__info">{JSON.stringify(rangeLogEvent.event, null, 2)}</td>
+                <td className="range-log__cell range-log__info">{JSON.stringify(rangeLogEvent.pretty_info, null, 2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -104,6 +141,10 @@ class DataDistribution extends React.Component<DataDistributionProps> {
             rows={dbTree}
             getValue={this.getCellValue}
           />
+          <h3>Range Log</h3>
+          <div className="data-distribution__range-log">
+            {this.renderRangeLog()}
+          </div>
         </div>
       </div>
     );
@@ -114,23 +155,28 @@ interface DataDistributionPageProps {
   dataDistribution: DataDistributionResponse;
   localityTree: LocalityTree;
   sortedZoneConfigs: ZoneConfig$Properties[];
+  rangeLog: RangeLogResponseMessage;
   refreshDataDistribution: typeof refreshDataDistribution;
   refreshNodes: typeof refreshNodes;
   refreshLiveness: typeof refreshLiveness;
+  refreshRangeLog: typeof refreshRangeLog;
 }
 
 class DataDistributionPage extends React.Component<DataDistributionPageProps> {
 
   componentDidMount() {
-    this.props.refreshDataDistribution();
-    this.props.refreshNodes();
-    this.props.refreshLiveness();
+    this.refresh();
   }
 
   componentWillReceiveProps() {
+    this.refresh();
+  }
+
+  refresh() {
     this.props.refreshDataDistribution();
     this.props.refreshNodes();
     this.props.refreshLiveness();
+    this.props.refreshRangeLog(new RangeLogRequest({ range_id: Long.fromInt(0), limit: 20 }));
   }
 
   render() {
@@ -145,13 +191,14 @@ class DataDistributionPage extends React.Component<DataDistributionPageProps> {
         <section className="section">
           <Loading
             className="loading-image loading-image__spinner-left"
-            loading={!this.props.dataDistribution || !this.props.localityTree}
+            loading={!this.props.dataDistribution || !this.props.localityTree || !this.props.rangeLog}
             image={spinner}
           >
             <DataDistribution
               localityTree={this.props.localityTree}
               dataDistribution={this.props.dataDistribution}
               sortedZoneConfigs={this.props.sortedZoneConfigs}
+              rangeLog={this.props.rangeLog}
             />
           </Loading>
         </section>
@@ -176,11 +223,13 @@ const DataDistributionPageConnected = connect(
     dataDistribution: state.cachedData.dataDistribution.data,
     sortedZoneConfigs: sortedZoneConfigs(state),
     localityTree: selectLocalityTree(state),
+    rangeLog: state.cachedData.rangeLog[0] ? state.cachedData.rangeLog[0].data : null,
   }),
   {
     refreshDataDistribution,
     refreshNodes,
     refreshLiveness,
+    refreshRangeLog,
   },
 )(DataDistributionPage);
 
